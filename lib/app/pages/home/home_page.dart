56de248f327e30e57cf5bc2ce26f9fec1bc1ab28
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2023-06-05 14:39:55 -0300

    feat: add logout in de home

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fwc_album_app/app/core/routes/routes_app.dart';
+import 'package:fwc_album_app/app/core/ui/widgets/button.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,7 +12,18 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home page'),
       ),
-      body: Container(),
+      body: Center(
+        child: Button.primary(
+            label: "logout",
+            onPressed: () async {
+              final sp = await SharedPreferences.getInstance();
+              sp.clear();
+              // ignore: use_build_context_synchronously
+              Navigator.of(context).pushNamedAndRemoveUntil(
+                  // ignore: use_build_context_synchronously
+                  context.routes.login, (route) => false);
+            }),
+      ),
     );
   }
 }
