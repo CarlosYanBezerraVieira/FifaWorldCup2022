b492ec9562e6a0b38016037037506890b2ec1010
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2023-06-06 15:15:46 -0300

    feat: created new interceptor for the custom dio

--- lib/app/core/rest/custom_dio.dart
+++ lib/app/core/rest/custom_dio.dart
@@ -1,21 +1,29 @@
-
 import 'package:dio/dio.dart';
 import 'package:dio/io.dart';
 import 'package:fwc_album_app/app/core/config/env/env.dart';
+import 'package:fwc_album_app/app/core/rest/interceptors/auth_interceptor.dart';
 
 class CustomDio extends DioForNative {
-  CustomDio(): super(BaseOptions(
-    baseUrl: Env.i['backend_base_url']?? '',
-    connectTimeout: const Duration(seconds: 5),
-    receiveTimeout:  const Duration(minutes: 1))){
-    interceptors.add(LogInterceptor(responseBody: true, requestBody: true,));
+  final _authInterceptor = AuthInterceptor();
+  CustomDio()
+      : super(BaseOptions(
+            baseUrl: Env.i['backend_base_url'] ?? '',
+            connectTimeout: const Duration(seconds: 5),
+            receiveTimeout: const Duration(minutes: 1))) {
+    interceptors.add(LogInterceptor(
+      responseBody: true,
+      requestBody: true,
+      requestHeader: true,
+    ));
   }
 
-  CustomDio auth(){
+  CustomDio auth() {
+    interceptors.add(_authInterceptor);
     return this;
   }
 
-    CustomDio unAuth(){
+  CustomDio unAuth() {
+    interceptors.remove(_authInterceptor);
     return this;
   }
 }
