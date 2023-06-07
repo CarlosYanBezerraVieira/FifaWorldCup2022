566a372e17093c5c06369833155aeb3224a5a198
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2023-06-07 20:04:24 -0300

    feat: created grid of stickers

--- lib/app/pages/my_sticker/widgets/sticker_group.dart
+++ lib/app/pages/my_sticker/widgets/sticker_group.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:fwc_album_app/app/core/ui/styles/colors_app.dart';
+
 import 'package:fwc_album_app/app/core/ui/styles/text_styles.dart';
 
 class StickerGroup extends StatelessWidget {
@@ -36,8 +39,71 @@ class StickerGroup extends StatelessWidget {
               style: context.textStyle.titleBlack.copyWith(fontSize: 26),
             ),
           ),
-          
+          GridView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: 20,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 4,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+            ),
+            itemBuilder: (context, index) {
+              return Sticker(
+                index: index,
+              );
+            },
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class Sticker extends StatelessWidget {
+  final int index;
+  const Sticker({
+    Key? key,
+    required this.index,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        decoration: BoxDecoration(
+            color:
+                index % 2 == 0 ? context.colors.primary : context.colors.grey),
+        child: Column(
+          children: [
+            Visibility(
+              maintainAnimation: true,
+              maintainState: true,
+              maintainSize: true,
+              visible: index % 2 == 0,
+              child: Container(
+                padding: const EdgeInsets.all(2),
+                alignment: Alignment.topRight,
+                child: Text(
+                  '1',
+                  style: context.textStyle.textSecondaryFontMedium
+                      .copyWith(color: context.colors.yellow),
+                ),
+              ),
+            ),
+            Text(
+              "BRA",
+              style: context.textStyle.textSecondaryFontExtraBold.copyWith(
+                  color: index % 2 == 0 ? Colors.white : Colors.black),
+            ),
+            Text(
+              "$index",
+              style: context.textStyle.textSecondaryFontExtraBold.copyWith(
+                  color: index % 2 == 0 ? Colors.white : Colors.black),
+            ),
+          ],
+        ),
       ),
     );
   }
