dd0ffa63f521ab81f284c2610c5c26b609829c7a
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2023-06-08 15:58:42 -0300

    feat: move file and implemented data of group sticker

--- lib/app/pages/my_stickers/widgets/sticker_group.dart
+++ lib/app/pages/my_stickers/widgets/sticker_group.dart
@@ -1,11 +1,20 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
-import 'package:fwc_album_app/app/core/ui/styles/colors_app.dart';
 
+import 'package:fwc_album_app/app/core/enum/sticker_status_enum.dart';
+import 'package:fwc_album_app/app/core/ui/styles/colors_app.dart';
 import 'package:fwc_album_app/app/core/ui/styles/text_styles.dart';
+import 'package:fwc_album_app/app/models/group_stickers.dart';
+import 'package:fwc_album_app/app/models/user_sticker_model.dart';
 
 class StickerGroup extends StatelessWidget {
-  const StickerGroup({super.key});
+  final StatusFilterEnum statusFilter;
+  final GroupStickers group;
+  const StickerGroup({
+    Key? key,
+    required this.statusFilter,
+    required this.group,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -24,8 +33,8 @@ class StickerGroup extends StatelessWidget {
                   alignment: const Alignment(0, -0.1),
                   widthFactor: 1,
                   heightFactor: 0.1,
-                  child: Image.asset(
-                    "assets/images/flags/BRA.png",
+                  child: Image.network(
+                    group.flag,
                     cacheWidth: (MediaQuery.of(context).size.width * 3).toInt(),
                   ),
                 ),
@@ -35,7 +44,7 @@ class StickerGroup extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(10.0),
             child: Text(
-              "Brasil",
+              group.countryName,
               style: context.textStyle.titleBlack.copyWith(fontSize: 26),
             ),
           ),
@@ -49,9 +58,29 @@ class StickerGroup extends StatelessWidget {
               mainAxisSpacing: 10,
             ),
             itemBuilder: (context, index) {
-              return Sticker(
-                index: index,
+              final stickerNumber = '${group.stickerStart + index}';
+              final stickerList = group.stickers
+                  .where((sticker) => sticker.stickerNumber == stickerNumber);
+              final sticker = stickerList.isNotEmpty ? stickerList.first : null;
+
+              final stickerWidget = Sticker(
+                stickerNumber: stickerNumber,
+                sticker: sticker,
+                countryCode: group.countryCode,
               );
+
+              if (statusFilter == StatusFilterEnum.all) {
+                return stickerWidget;
+              } else if (statusFilter == StatusFilterEnum.missing) {
+                if (sticker == null) {
+                  return stickerWidget;
+                }
+              } else if (statusFilter == StatusFilterEnum.repeated) {
+                if (sticker != null && sticker.duplicate > 0) {
+                  return stickerWidget;
+                }
+              }
+              return const SizedBox.shrink();
             },
           ),
         ],
@@ -61,10 +90,15 @@ class StickerGroup extends StatelessWidget {
 }
 
 class Sticker extends StatelessWidget {
-  final int index;
+  final String stickerNumber;
+
+  final String countryCode;
+  final UserStickerModel? sticker;
   const Sticker({
     Key? key,
-    required this.index,
+    required this.stickerNumber,
+    required this.countryCode,
+    required this.sticker,
   }) : super(key: key);
 
   @override
@@ -73,34 +107,35 @@ class Sticker extends StatelessWidget {
       onTap: () {},
       child: Container(
         decoration: BoxDecoration(
-            color:
-                index % 2 == 0 ? context.colors.primary : context.colors.grey),
+            color: (sticker?.duplicate ?? 0) > 0
+                ? context.colors.primary
+                : context.colors.grey),
         child: Column(
           children: [
             Visibility(
               maintainAnimation: true,
               maintainState: true,
               maintainSize: true,
-              visible: index % 2 == 0,
+              visible: sticker != null,
               child: Container(
                 padding: const EdgeInsets.all(2),
                 alignment: Alignment.topRight,
                 child: Text(
-                  '1',
+                  sticker?.duplicate.toString() ?? '',
                   style: context.textStyle.textSecondaryFontMedium
                       .copyWith(color: context.colors.yellow),
                 ),
               ),
             ),
             Text(
-              "BRA",
+              countryCode,
               style: context.textStyle.textSecondaryFontExtraBold.copyWith(
-                  color: index % 2 == 0 ? Colors.white : Colors.black),
+                  color: sticker != null ? Colors.white : Colors.black),
             ),
             Text(
-              "$index",
+              stickerNumber,
               style: context.textStyle.textSecondaryFontExtraBold.copyWith(
-                  color: index % 2 == 0 ? Colors.white : Colors.black),
+                  color: sticker != null ? Colors.white : Colors.black),
             ),
           ],
         ),
